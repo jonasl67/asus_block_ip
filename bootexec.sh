1893734fc7723119f0b9080e0a33d2416a7cf1a2
Jonas Lindeborg <jonas.lindeborg@mblox.com>
2018-02-04 17:31:53 +0100

    Update bootexec.sh

--- bootexec.sh
+++ bootexec.sh
@@ -23,19 +23,17 @@ if [ -f $ISBLOCKEDFILE ]; then
 fi
 
 # update cron to block/unblock per schedule below
-#/usr/sbin/cru a unblockweekdays "0 20 * * 1-4 /jffs/blocking.sh unblock weekday"
-#/usr/sbin/cru a blockweekdays "0 21 * * 1-4 /jffs/blocking.sh block weekday"
-/usr/sbin/cru a unblockweekdays "0 17 * * 1-4 /jffs/blocking.sh unblock weekday_schoolbreak"
-/usr/sbin/cru a blockweekdays "0 1 * * 2-5 /jffs/blocking.sh block weekday_schoolbreak"
+/usr/sbin/cru a unblockweekdays "30 19 * * 1-4 /jffs/blocking.sh unblock weekday"
+/usr/sbin/cru a blockweekdays "0 21 * * 1-4 /jffs/blocking.sh block weekday"
 /usr/sbin/cru a unblockfri "0 16 * * 5 /jffs/blocking.sh unblock Friday"
 /usr/sbin/cru a blockfri "0 1 * * 6 /jffs/blocking.sh block Friday"
-/usr/sbin/cru a unblocksat "0 16 * * 6 /jffs/blocking.sh unblock Saturday"
+/usr/sbin/cru a unblocksat "0 17 * * 6 /jffs/blocking.sh unblock Saturday"
 /usr/sbin/cru a blocksat "0 1 * * 0 /jffs/blocking.sh block Saturday"
-/usr/sbin/cru a unblocksun "0 16 * * 0 /jffs/blocking.sh unblock Sunday"
-/usr/sbin/cru a blocksun "0 20 * * 0 /jffs/blocking.sh block Sunday"
+/usr/sbin/cru a unblocksun "0 17 * * 0 /jffs/blocking.sh unblock Sunday"
+/usr/sbin/cru a blocksun "0 21 * * 0 /jffs/blocking.sh block Sunday"
 
-# add watchdog every second minute, this is because the router seems to flush/reset iptables at some frequent / random (?) interval
-/usr/sbin/cru a blockwatchdog "*/2 * * * * /jffs/blocking.sh checkBlocking"
+# add watchdog every minute, this is because the router seems to flush/reset iptables at some frequent / random (?) interval
+/usr/sbin/cru a blockwatchdog "*/1 * * * * /jffs/blocking.sh checkBlocking"
 
 # report completion
 echo "$(date) Boot (/jfss/bootexec.sh), completed normally " >> $LOGFILE
